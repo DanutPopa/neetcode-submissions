1ee692864db096e1bd6f7f8fd19c95536aabf624
Danut Popa <108424517+DanutPopa@users.noreply.github.com>
2026-08-19 20:53:03 +0300

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        var count = 0
+        var maxConsecutiveOnes = 0
+
+        for num in nums {
+            if num == 1 {
+                count += 1
+            } 
+            else {
+                count = 0
+            }
+            if count > maxConsecutiveOnes {
+                maxConsecutiveOnes = count
+            }
+        }
+        return maxConsecutiveOnes
+    }
+}
