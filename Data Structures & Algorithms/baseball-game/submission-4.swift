c0674f397a2d5134c2b0d9f785692d15a25f376d
Danut Popa <108424517+DanutPopa@users.noreply.github.com>
2026-09-08 18:53:24 +0300

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func calPoints(_ operations: [String]) -> Int {
+        var records: [Int] = []
+
+        for operation in operations {
+            if let record = Int(operation) {
+                records.append(record)
+            }
+            else {
+                switch operation {
+                    case "+":
+                    var sum = 0
+                    for i in stride(from: records.count - 1, through: records.count - 2, by: -1) {
+                        sum += records[i]
+                    }
+                    records.append(sum)
+                    case "C":
+                    records.removeLast()
+                    case "D":
+                    if let last = records.last {
+                        records.append(last * 2)
+                    }
+                    default:
+                    break
+                }
+            }
+        }
+        return records.reduce(0, +)
+    }
+}
